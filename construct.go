7f4471c19ac1f7b711076c40aca9e287f84bd3fe
user <user@mail.com>
2024-12-15 22:53:56 -0500

    used for downstream

--- construct.go
+++ construct.go
@@ -40,7 +40,7 @@ func ConstructPaywall() (*Paywall, error) {
 		PriceInBTC:     0.0001,    // 0.0001 BTC
 		TestNet:        false,     // Use testnet
 		Store:          fileStore, // Required for payment tracking
-		PaymentTimeout: time.Minute * 45,
+		PaymentTimeout: time.Hour * 2,
 	})
 	if err != nil {
 		return nil, err
